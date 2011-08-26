c05463500d4c4301044e0b2340c07bd52c805c00
Janko Marohnić <janko.marohnic@gmail.com>
2011-08-26 03:55:16 +0200

    switched to Sequel, organized a lot

--- app.rb
+++ app.rb
@@ -1,27 +1,25 @@
 # encoding:utf-8
 require 'sinatra'
-require 'sinatra/activerecord'
-require 'haml'
-require 'sass'
-require 'compass'
+require_relative 'config'
 
-require_relative 'configs/config'
-require_relative 'configs/cro_dates'
+require_relative 'extras/cro_dates'
 
-class Post < ActiveRecord::Base
-end
+Pages = [
+  {normal: 'index', croatian: 'Početna'},
+  {normal: 'o_nama', croatian: 'O Nama'},
+  {normal: 'slike', croatian: 'Slike'},
+  {normal: 'video', croatian: 'Video'}
+]
 
 helpers do
   def current?(page)
     (('/' + page[:normal]) == request.path_info)
   end
+end
 
-  Pages = [
-    {normal: 'index', croatian: 'Početna'},
-    {normal: 'o_nama', croatian: 'O Nama'},
-    {normal: 'slike', croatian: 'Slike'},
-    {normal: 'video', croatian: 'Video'}
-  ]
+get '/delete' do
+  Post.order(:id).last.delete
+  redirect :index
 end
 
 get '/' do
@@ -32,25 +30,27 @@ get '/:page' do
   haml params[:page].to_sym
 end
 
-get '/edit/:id' do
-  @post = Post.find(params[:id].to_i)
-  haml :izmjeni
+get '/post/new' do
+  @post = Post.new
+  haml :post
 end
 
-put '/:id' do
-  @post = Post.find(params[:id].to_i)
-  @post.title, @post.subtitle, @post.body = params[:title], params[:subtitle], params[:body]
-  @post.save
+get '/post/:id' do
+  @post = Post[params[:id]]
+  haml :post
+end
+
+put '/edit/' do
+  post = Post[params[:id]] || Post.new
+  post.title, post.subtitle = params[:title], params[:subtitle]
+  post.body = params[:body]
+  post.created_at = Date.today unless post.id
+  post.save
   redirect :index
 end
 
-get '/index/:id' do
-  @post_id = params[:id].to_i
-  haml :index
-end
-
-delete '/:id' do
-  Post.delete(params[:id].to_i)
+delete '/delete/:id' do
+  Post[params[:id]].delete
   redirect :index
 end
 
