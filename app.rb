cffbc7d45f0c87c3bab834afad9ca3be585dddfa
Matija Marohnić <matija.marohnic@gmail.com>
2011-09-19 13:21:06 +0200

    Fix bug with Compass image paths

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ configure do
   Compass.configuration do |config|
     config.sass_dir = 'views/css'
     config.images_dir = 'public/images'
+    config.http_images_path = File.join(config.http_path, 'images')
     config.line_comments = false
   end
 end
