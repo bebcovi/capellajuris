f0ab6f884730b815dceb7e86170a1ef734af5dd6
Janko Marohnić <janko.marohnic@gmail.com>
2011-10-07 22:31:58 +0200

    Remove unnecessary migration files

--- db/migrate/20111006194140_change_date_to_timestamps.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDateToTimestamps < ActiveRecord::Migration
-  def up
-    change_column :news, :created_at, :timestamp
-  end
-
-  def down
-    change_column :news, :created_at, :date
-  end
-end
