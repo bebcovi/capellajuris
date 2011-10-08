ca562f988dd4fade3a71726eb953fdcea1f08521
Matija Marohnić <matija.marohnic@gmail.com>
2011-10-08 03:56:03 +0200

    arrange js assets into proper order

--- app.rb
+++ app.rb
@@ -52,7 +52,7 @@ require 'db/models'
 enable :sessions
 
 # Sinatra Boilerplate
-set :js_assets, %w[js/post.coffee, js/init.coffee, js/gollum.editor.js, js/markdown.js]
+set :js_assets, %w[js/gollum.editor.js, js/markdown.js, js/post.coffee, js/init.coffee]
 
 
 # Javascript
