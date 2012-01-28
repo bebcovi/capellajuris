8b0aa6c1147dfb42d0656e62306c414fdb6d3f3f
Janko Marohnić <janko.marohnic@gmail.com>
2012-01-28 21:16:20 +0100

    Fix the iframe error even if YouTube changes double quotes to single quotes

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,10 @@ class Video < ActiveRecord::Base
   validates_presence_of :link, :message => "YouTube video ne smije biti prazan"
 
   before_create do
-    self.link = link.gsub(%["], %['])
+    if link =~ /"/
+      self.link = link.gsub(%["], %['])
+    elsif link =~ /'/
+      self.link = link.gsub(%['], %["])
+    end
   end
 end
