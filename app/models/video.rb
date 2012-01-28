3bf64c4c2b4fe1699aad8b9543e58aa887cda29a
Janko Marohnić <janko.marohnic@gmail.com>
2012-01-28 17:47:09 +0100

    Fix iframe bug

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,7 @@
 class Video < ActiveRecord::Base
   validates_presence_of :link, :message => "YouTube video ne smije biti prazan"
 
+  before_create do
+    self.link = link.gsub(%["], %['])
+  end
 end
