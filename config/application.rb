044967c8723eaceebc61f72062f4f21ffe89a13c
Janko Marohnić <janko.marohnic@gmail.com>
2012-01-31 22:48:56 +0100

    Tranfered to the choir's flickr account

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ end
 
 module CapellaJuris
   class Application < Rails::Application
-    config.admin = {:username => "capella", :password => "juris", :flickr_set => 72157627460265059}
+    config.admin = {:username => "capella", :password => "juris", :flickr_set => 72157628108442555}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
