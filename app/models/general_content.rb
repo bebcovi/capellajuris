b2c9002718e25d5e3313eb87ebb544b7bc899ea6
Janko Marohnić <janko.marohnic@gmail.com>
2012-02-02 22:50:24 +0100

    Add validations for GeneralContent

--- app/models/general_content.rb
+++ app/models/general_content.rb
@@ -1,2 +1,5 @@
+# encoding: utf-8
 class GeneralContent < ActiveRecord::Base
+  validates_presence_of :title, :message => "Naslov ne smije biti prazan"
+  validates_presence_of :text, :message => "Sadržaj ne smije biti prazan"
 end
