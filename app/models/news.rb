cc0aaca7ec44cfb54c73cad8c8a9a96d817ff7f4
Janko Marohnić <janko.marohnic@gmail.com>
2012-02-25 18:38:49 +0100

    Delete old news when their number surpasses 100

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,4 +2,8 @@
 class News < ActiveRecord::Base
   validates_presence_of :title, :message => "Naslov ne smije biti prazan."
   validates_presence_of :text, :message => "Sadržaj ne smije biti prazan."
+
+  after_create do
+    self.class.order(:created_at).first.destroy if self.class.count > 100
+  end
 end
--- test/unit/news_test.rb
+++ test/unit/news_test.rb
@@ -5,4 +5,11 @@ class NewsTest < ActiveSupport::TestCase
     assert !News.new(:text => "Some markdown").save, "News with blank title was saved."
     assert !News.new(:title => "Title").save, "News with blank text was saved."
   end
+
+  test "callbacks" do
+    100.times { |n| News.create(:title => "Title #{n}", :text => "Text #{n}", :created_at => n.minutes.from_now) }
+    assert_not_nil News.find_by_title("Title 0")
+    News.create(:title => "Title 101", :text => "Text 101")
+    assert_nil News.find_by_title("Title 0")
+  end
 end
