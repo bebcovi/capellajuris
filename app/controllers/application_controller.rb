5b18de26f69b03518d06903d49845bd6e3f89101
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-01 15:49:30 +0100

    Fix some things about staging environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  around_filter :catch_exceptions, :if => proc { Rails.env.production? }
+  around_filter :catch_exceptions, :if => proc { Rails.env.production? || Rails.env.staging? }
 
 protected
 
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -57,6 +57,4 @@ CapellaJuris::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.middleware.use ErrorPagesGenerator
 end
