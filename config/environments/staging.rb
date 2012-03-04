c7d6cc0c3862b720bd122ebf7077ec098a8a53c1
Janko Marohnić <janko.marohnic@gmail.com>
2012-03-04 19:42:43 +0100

    Improve staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -9,13 +9,10 @@ CapellaJuris::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.serve_static_assets = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -60,4 +57,8 @@ CapellaJuris::Application.configure do
 
   # Use dynamic HTTP-error pages instead of static ones
   config.exceptions_app = self.routes
+
+  config.sass.preferred_syntax = :sass
+  config.compass.line_comments = false
+  config.compass.images_dir = File.join('app', 'assets', 'images')
 end
